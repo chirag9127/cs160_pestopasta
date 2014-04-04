2fd45f4bac67eb5936c4974d40af11732bebdd7e
Chirag Mahapatra <chirag.mahapatra@berkeley.edu>
2014-04-04 16:00:14 -0700

    maps

--- src/com/example/cs160project/MainActivity.java
+++ src/com/example/cs160project/MainActivity.java
@@ -1,22 +1,49 @@
 package com.example.cs160project;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	
+	private GoogleMap googleMap;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
+	@SuppressLint("NewApi")
+	private void initilizeMap() {
+        if (googleMap == null) {
+        	googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
 
 }
