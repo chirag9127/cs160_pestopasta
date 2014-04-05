9125cfc503a5a137ab3a418887340ec5f31c5f6a
Chirag Mahapatra <chirag.mahapatra@berkeley.edu>
2014-04-05 16:19:07 -0700

    playback

--- src/com/example/cs160project/MainActivity.java
+++ src/com/example/cs160project/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.cs160project;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -40,14 +43,26 @@ public class MainActivity extends Activity {
                         .show();
             }else{
             	// latitude and longitude
-            	double latitude = 17.385044;
-            	double longitude = 78.486671;
+            	double latitude = 37.8717;
+            	double longitude = 122.2728;
             	 
             	// create marker
-            	MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
+            	Marker marker = googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps "));
             	 
             	// adding marker
-            	googleMap.addMarker(marker);
+            	//googleMap.addMarker(marker);
+            	
+            	googleMap.setOnMarkerClickListener(new OnMarkerClickListener(){
+
+					@Override
+					public boolean onMarkerClick(Marker arg0) {
+						arg0.showInfoWindow();
+						Intent intent= new Intent(MainActivity.this, PlayBackActivity.class);
+                        startActivity(intent);
+						return false;
+					}
+            		
+            	});
             }
         }
     }
